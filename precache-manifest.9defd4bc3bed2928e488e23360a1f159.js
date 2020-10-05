709802082e6465047840b9bc0f88ffc9bc247558
danish-mehmood <rdm355190@gmail.com>
2020-10-05 19:04:42 +0100

    Updates

---
+++ precache-manifest.9defd4bc3bed2928e488e23360a1f159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a993230359a6a369aef643c68ad166",
+    "url": "/danish-mehmood.github.io/index.html"
+  },
+  {
+    "revision": "feff84a19599dfa065e5",
+    "url": "/danish-mehmood.github.io/static/css/main.15fe3dbc.chunk.css"
+  },
+  {
+    "revision": "c069b7586872ef9c2df7",
+    "url": "/danish-mehmood.github.io/static/js/2.62fa569c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/danish-mehmood.github.io/static/js/2.62fa569c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feff84a19599dfa065e5",
+    "url": "/danish-mehmood.github.io/static/js/main.e764fb98.chunk.js"
+  },
+  {
+    "revision": "7a54e6abf82ded3df3d7",
+    "url": "/danish-mehmood.github.io/static/js/runtime-main.aeb3daa6.js"
+  }
+]);
